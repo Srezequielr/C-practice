14fb957015656227cccf5ed2a23f8a5382d3ff2d
Srezequielr <julianrierawrk@gmail.com>
2024-11-18 17:26:06 -0300

    4.9 Sin hacer

---
+++ PProcedural/U4/4.9 ministerio/4.9 ministerio.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// El Ministerio de Producción ha lanzado un Plan de Promoción de Capacitación de Empleados (PPCE) para las PYMES
+// (Pequeñas y Medianas Empresas). La siguiente tabla detalla los montos financiados para el año actual (expresados
+// en millones de pesos) según las distintas categorías/sectores, lo que permite clasificar cada una de las empresas.
+// a) Indicar el monto total financiado para una categoría ingresada por teclado.
+// b) Indicar el monto total financiado para el sector de Servicios, sin importar la categoría de la empresa.
+// c) Emitir un listado con el total financiado, sin importar la categoría/ sector.
+// d) Emitir un listado con los montos superiores a uno ingresado por teclado, y a continuación los inferiores e iguales,
+// indicando sector y categoría.
+
+int main()
+{
+    
+}
