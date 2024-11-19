ac11febfc95d408fd29118d0f342f883746b93f9
Srezequielr <julianrierawrk@gmail.com>
2024-11-19 15:22:56 -0300

    Ej 5.2

--- PProcedural/U5/5.2 socios/5.2 socios.cpp
+++ PProcedural/U5/5.2 socios/5.2 socios.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct
 {
@@ -9,7 +10,7 @@ typedef struct
     char tipos;
 } Socio;
 
-int *cargaSocios(int N)
+Socio *cargaSocios(int N)
 {
 
     int i;
@@ -26,13 +27,105 @@ int *cargaSocios(int N)
         {
             printf("Ingrese DNI del socio: ");
             scanf("%d", &arre[i].DNI);
+            while (getchar() != '\n')
+                ;
             printf("Ingrese edad del socio: ");
             scanf("%d", &arre[i].edad);
+            while (getchar() != '\n')
+                ;
             printf("Ingese tipo de socio ('A': Socio deportivo, 'J': Socio Jubilado): ");
-            scanf("%c", &arre[i].estado);
+            scanf("%c", &arre[i].tipos);
+            while (getchar() != '\n')
+                ;
             printf("Ingrese estado del socio (Activo o Inactivo): ");
             scanf("%s", &arre[i].estado);
+            while (getchar() != '\n')
+                ;
         }
+        printf("----- Fin de carga -----\n");
+    }
+    return arre;
+}
+
+Socio *cargaSubArre(Socio *socios, int N, int &O)
+{
+    int cont = 0, i;
+    O = 0;
+    char tipo;
+    Socio *subArre;
+    printf("Ingrese tipo de socio para seleccionar ('A': Socio deportivo, 'J': Socio Jubilado): ");
+    scanf("%c", &tipo);
+    while (getchar() != '\n')
+        ;
+    for (i = 0; i < N; i++)
+    {
+        if (socios[i].tipos == tipo && socios[i].edad > 40)
+        {
+            cont++;
+        }
+    }
+
+    subArre = (Socio *)malloc(cont * sizeof(Socio));
+
+    if (subArre == NULL)
+    {
+        printf("No se pudo crear el arreglo\n");
+    }
+    else
+    {
+        for (i = 0; i < N; i++)
+        {
+            if (socios[i].tipos == tipo && socios[i].edad > 40)
+            {
+                subArre[O] = socios[i];
+                O++;
+            }
+        }
+    }
+    return subArre;
+}
+
+Socio *activos(Socio *socios, int N, int &P, char tipo)
+{
+    Socio *arreglo;
+    P = 0;
+    int i, cont = 0;
+
+    for (i = 0; i < N; i++)
+    {
+        if (socios[i].tipos == tipo && strcmp(socios[i].estado, "Activo") == 0)
+        {
+            cont++;
+        }
+    }
+
+    arreglo = (Socio *)malloc(cont * sizeof(Socio));
+
+    if (arreglo == NULL)
+    {
+        printf("No se pudo crear el arreglo\n");
+    }
+    else
+    {
+        for (i = 0; i < N; i++)
+        {
+            if (socios[i].tipos == tipo && strcmp(socios[i].estado, "Activo") == 0)
+            {
+                arreglo[P] = socios[i];
+                P++;
+            }
+        }
+    }
+    return arreglo;
+}
+
+void mostrarSocios(Socio *socios, int N)
+{
+    int i;
+
+    for (i = 0; i < N; i++)
+    {
+        printf("- DNI: %d\n", socios[i].DNI);
     }
 }
 
@@ -49,10 +142,21 @@ int *cargaSocios(int N)
 
 int main()
 {
-    int N;
-    Socio *socios;
+    int N, O, J, D;
+    Socio *socios, *subSocios, *aJubilados, *aDeportivo;
 
     printf("Ingrese la cantidad de socios: ");
     scanf("%d", &N);
-    
+    socios = cargaSocios(N);
+    subSocios = cargaSubArre(socios, N, O);
+    printf("------ DNIs de socios de tipo seleccionado ------\n");
+    mostrarSocios(subSocios, O);
+
+    aJubilados = activos(socios, N, J, 'J');
+    printf("------ DNIs de socios de jubilados activos ------\n");
+    mostrarSocios(aJubilados, J);
+
+    aDeportivo = activos(socios, N, D, 'A');
+    printf("------ DNIs de socios de deportivos activos ------\n");
+    mostrarSocios(aJubilados, D);
 }
